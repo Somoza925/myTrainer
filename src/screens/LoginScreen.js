4993384242ac92ed957a9d213972844bac09ab1b
Somoza <Somoza0315@gmail.com>
2019-10-25 17:25:00 -0400

    Fixed login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -3,12 +3,12 @@ import { View, Text, StyleSheet, TextInput } from 'react-native';
 import { Button } from 'react-native-elements';
 
 const LoginScreen = ({ navigation }) => {
-    state = {
-        username: '',
-        password: '',
-        errorMessage: null
+	state = {
+		username: '',
+		password: '',
+		errorMessage: null
 	}
-  
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.card}>
@@ -16,13 +16,13 @@ const LoginScreen = ({ navigation }) => {
 				<TextInput
 					style={styles.inputField}
 					placeholder='name...'
-					onChangeText={(username) => this.setState({username})}
+					onChangeText={(username) => this.setState({ username })}
 					value={this.state.username}
 				/>
 				<TextInput
 					style={styles.inputField}
 					placeholder='Password...'
-					onChangeText={(password) => this.setState({password})}
+					onChangeText={(password) => this.setState({ password })}
 					value={this.state.password}
 				/>
 				<View style={styles.buttonRow}>
@@ -40,47 +40,49 @@ const LoginScreen = ({ navigation }) => {
 					/>
 				</View>
 			</View>
-
+		</View>
+	);
+};
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#ffffff',
-        paddingHorizontal: 75,
-        paddingVertical: 175,
-        flex: 1
-    },
-    card: {
-        backgroundColor: '#ffffff',
-        borderRadius: 5,
-        shadowOpacity: 5,
-        shadowColor: 'gray',
-        shadowOffset: {
-            width: -3,
-            height: 3
-        },
-        padding: 5
-    },
-    title: {
-        fontSize: 46,
-        textAlign: 'center',
-        paddingBottom: 30,
-    },
-    inputField: {
-        height: 35,
-        paddingLeft: 5,
-        marginBottom: 5,
-        borderWidth: 0.5,
-        borderRadius: 5,
-        borderColor: 'gray'
-    },
-    buttonRow: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingHorizontal: 40,
-        paddingTop: 10,
-    },
-    buttons: {
-        flex: 1,
-        // borderWidth: 1
+	container: {
+		backgroundColor: '#ffffff',
+		paddingHorizontal: 75,
+		paddingVertical: 175,
+		flex: 1
+	},
+	card: {
+		backgroundColor: '#ffffff',
+		borderRadius: 5,
+		shadowOpacity: 5,
+		shadowColor: 'gray',
+		shadowOffset: {
+			width: -3,
+			height: 3
+		},
+		padding: 5
+	},
+	title: {
+		fontSize: 46,
+		textAlign: 'center',
+		paddingBottom: 30,
+	},
+	inputField: {
+		height: 35,
+		paddingLeft: 5,
+		marginBottom: 5,
+		borderWidth: 0.5,
+		borderRadius: 5,
+		borderColor: 'gray'
+	},
+	buttonRow: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		paddingHorizontal: 40,
+		paddingTop: 10,
+	},
+	buttons: {
+		flex: 1,
+		// borderWidth: 1
 	},
 });
 
