c1a8cf8e7f0532a2dcbf94f6338d52a98e26b1a9
Christian Sarmiento <christiansarmiento@Christians-MacBook-Pro.local>
2019-10-25 16:43:49 -0400

    Fixed all indentations

--- src/screens/ExploreScreen.js
+++ src/screens/ExploreScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 
 const ExploreScreen = () => {
-    return(
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Text> This is my Explore screen </Text>
-        </View>
-      );
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text> This is my Explore screen </Text>
+		</View>
+	);
 }
 
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 
 const HomeScreen = () => {
-    return(
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Text> This is my Home screen </Text>
-        </View>
-      );
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text> This is my Home screen </Text>
+		</View>
+	);
 }
 
 
