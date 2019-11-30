1674d9022015ffae1ac7ede7eb098a855c76bd6b
NavidGhomeshi <nghomeshi@ufl.edu>
2019-11-30 18:39:24 -0500

    fixed several things: Added workouts.json and meals.json, integrated to home page, fixed addPartner check to profile screen to se if email exists, added create chat with partner to chat screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,72 +1,87 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, Picker } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, Picker, Button } from 'react-native';
 import { ButtonGroup, Card, ListItem } from 'react-native-elements';
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import firebaseSDK from '../config/firebaseSDK';
 import Workouts from '../json files/workouts.json'
+import Meals from '../json files/meals.json'
 
 const HomeScreen = ({ navigation }) => {
 
 	const [selectedIndex, updateIndex] = useState(0);
 	const [workout, setWorkout] = useState(Workouts.chest);
+	const [food, setFood] = useState(Meals.meals1);
 	const buttons = ['Workout', 'Nutrition']
 
-	const userNutrition = [
-		{
-			name: 'Calories',
-			total: '1700 kcal',
-		},
-		{
-			name: 'Fat',
-			total: '60 g',
-		},
-		{
-			name: 'Protein',
-			total: '130 g',
-		},
-		{
-			name: 'Carbohydrates',
-			total: '180 g',
-		}
-	]
-
-	const tableHead = ['Meal', 'Calories', 'Protein', 'Fat', 'Carbs']
-	const tableTitle = ['Eggs', 'Banana', 'Pasta', 'Yogurt']
-	const tableData = [
-		['400', '30', '10', '40'],
-		['200', '10', '10', '20'],
-		['800', '70', '30', '100'],
-		['300', '20', '10', '20']
-	]
-
-console.log(workout);
+	const doIt = (workouts) => {
+		firebaseSDK.createWorkoutsFolder(workouts);
+	}
 
 	const workoutScreen = (selectedIndex) => {
 		if (selectedIndex === 0) {
 			return (
 				<SafeAreaView>
-					<Picker
+					{/* <Picker
 						selectedValue={workout}
-						onValueChange={(itemValue) => setWorkout(itemValue)}
-						>
-						<Picker.Item label="Chest" value={Workouts.chest} />
-						<Picker.Item label="Back" value={Workouts.back}  />
-					</Picker>
-					<Card title='Muscle Group: Chest' titleStyle={styles.cardTitle}>
+						onValueChange={(itemValue, itemPosition) => {setWorkout(itemValue), setChosenIndex(itemPosition), setWorkout2(itemValue)}}
+					>
+						<Picker.Item label="Chest" value={Workouts.chest} key={chosenIndex}/>
+						<Picker.Item label="Back" value={Workouts.back} key={chosenIndex}/>
+						<Picker.Item label="Legs" value={Workouts.legs} key={chosenIndex}/>
+						<Picker.Item label="Shoulders" value={Workouts.shoulders} key={chosenIndex} />
+						<Picker.Item label="Arms" value={Workouts.arms} key={chosenIndex}/>
+						
+					</Picker> */}
+
+					<SafeAreaView style={styles.buttonContainer}>
+						<Button
+							title='Chest'
+							type='solid'
+							style={styles.button}
+							onPress={() => setWorkout(Workouts.chest)}
+						></Button>
+						<Button
+							title='Back'
+							type='solid'
+							style={styles.button}
+							onPress={() => setWorkout(Workouts.back)}
+						></Button>
+						<Button
+							title='Legs'
+							type='solid'
+							style={styles.button}
+							onPress={() => setWorkout(Workouts.legs)}
+						></Button>
+						<Button
+							title='Shoulders'
+							type='solid'
+							style={styles.button}
+							onPress={() => setWorkout(Workouts.shoulders)}
+						></Button>
+						<Button
+							title='Arms'
+							type='solid'
+							style={styles.button}
+							onPress={() => setWorkout(Workouts.arms)}
+						></Button>
+					</SafeAreaView>
+
+					<Card title={workout[0].cardTitle} titleStyle={styles.cardTitle}>
+						<Text style={styles.workoutExercise} > {workout[0].name} </Text>
+						<Text style={styles.workoutDetails}>Sets: {workout[0].sets} </Text>
+						<Text style={styles.workoutDetails}>Reps: {workout[0].reps} </Text>
+
 						<Text style={styles.workoutExercise} > {workout[1].name} </Text>
 						<Text style={styles.workoutDetails}>Sets: {workout[1].sets} </Text>
 						<Text style={styles.workoutDetails}>Reps: {workout[1].reps} </Text>
 
-						<Text style={styles.workoutExercise} > Incline Dumbbell Press</Text>
-						<Text style={styles.workoutDetails}>Sets: 4</Text>
-						<Text style={styles.workoutDetails}>Repetitions: 12, 10, 8, 6</Text>
+						<Text style={styles.workoutExercise} > {workout[2].name} </Text>
+						<Text style={styles.workoutDetails}>Sets: {workout[2].sets} </Text>
+						<Text style={styles.workoutDetails}>Reps: {workout[2].reps} </Text>
 
-						<Text style={styles.workoutExercise} > Chest Fly Machine</Text>
-						<Text style={styles.workoutDetails}>Sets: 3</Text>
-						<Text style={styles.workoutDetails}>Repetitions: 15, 12, 10</Text>
-
-						<Text style={styles.workoutExercise} > Decline Cable Push</Text>
-						<Text style={styles.workoutDetails}>Sets: 3</Text>
-						<Text style={styles.workoutDetails}>Repetitions: 15, 12, 10</Text>
+						<Text style={styles.workoutExercise} > {workout[3].name} </Text>
+						<Text style={styles.workoutDetails}>Sets: {workout[3].sets} </Text>
+						<Text style={styles.workoutDetails}>Reps: {workout[3].reps} </Text>
 
 					</Card>
 				</SafeAreaView>
@@ -75,32 +90,78 @@ console.log(workout);
 		if (selectedIndex === 1) {
 			return (
 				<SafeAreaView>
-					<Card title='Daily Total' titleStyle={styles.cardTitle}>
-						{
-							userNutrition.map((u, i) => {
-								return (
-									<ListItem
-										key={i}
-										title={u.name}
-										rightTitle={u.total}
-									></ListItem>
-								)
-							})
-						}
-					</Card>
-					<Card titleStyle={styles.cardTitle}>
-						<Table>
-							<Row data={tableHead} flexArr={[1, 2, 1, 1]} style={styles.head} textStyle={styles.text} />
-							<TableWrapper style={styles.wrapper}>
-								<Col data={tableTitle} style={styles.title} heightArr={[28, 28]} textStyle={styles.text} />
-								<Rows data={tableData} flexArr={[2, 1, 1]} style={styles.row} textStyle={styles.text} />
-							</TableWrapper>
-						</Table>
+					<SafeAreaView style={styles.buttonContainer}>
+						<Button
+							title='Chicken'
+							type='solid'
+							style={styles.button}
+							onPress={() => setFood(Meals.meals1)}
+						></Button>
+						<Button
+							title='Red Meat'
+							type='solid'
+							style={styles.button}
+							onPress={() => setFood(Meals.meals2)}
+						></Button>
+						<Button
+							title='Veggie'
+							type='solid'
+							style={styles.button}
+							onPress={() => setWorkout(Workouts.legs)}
+						></Button>
+					</SafeAreaView>
+
+					<Card title={food[0].cardTitle} titleStyle={styles.cardTitle}>
+						<Text style={styles.workoutExercise} > {food[0].name} </Text>
+						<Text style={styles.workoutDetails}>Calories: {food[0].calories} </Text>
+						<Text style={styles.workoutDetails}>Fat: {food[0].fat} </Text>
+						<Text style={styles.workoutDetails}>Protein: {food[0].protein} </Text>
+						<Text style={styles.workoutDetails}>Carbs: {food[0].carbs} </Text>
+
+						<Text style={styles.workoutExercise} > {food[1].name} </Text>
+						<Text style={styles.workoutDetails}>Calories: {food[1].calories} </Text>
+						<Text style={styles.workoutDetails}>Fat: {food[1].fat} </Text>
+						<Text style={styles.workoutDetails}>Protein: {food[1].protein} </Text>
+						<Text style={styles.workoutDetails}>Carbs: {food[1].carbs} </Text>
+						
+						<Text style={styles.workoutExercise} > {food[2].name} </Text>
+						<Text style={styles.workoutDetails}>Calories: {food[2].calories} </Text>
+						<Text style={styles.workoutDetails}>Fat: {food[2].fat} </Text>
+						<Text style={styles.workoutDetails}>Protein: {food[2].protein} </Text>
+						<Text style={styles.workoutDetails}>Carbs: {food[2].carbs} </Text>
+
 					</Card>
 				</SafeAreaView>
 			)
+			// return (
+			// 	<SafeAreaView>
+			// 		<Card title='Daily Total' titleStyle={styles.cardTitle}>
+			// 			{
+			// 				userNutrition.map((u, i) => {
+			// 					return (
+			// 						<ListItem
+			// 							key={i}
+			// 							title={u.name}
+			// 							rightTitle={u.total}
+			// 						></ListItem>
+			// 					)
+			// 				})
+			// 			}
+			// 		</Card>
+			// 		<Card titleStyle={styles.cardTitle}>
+			// 			<Table>
+			// 				<Row data={tableHead} flexArr={[1, 2, 1, 1]} style={styles.head} textStyle={styles.text} />
+			// 				<TableWrapper style={styles.wrapper}>
+			// 					<Col data={tableTitle} style={styles.title} heightArr={[28, 28]} textStyle={styles.text} />
+			// 					<Rows data={tableData} flexArr={[2, 1, 1]} style={styles.row} textStyle={styles.text} />
+			// 				</TableWrapper>
+			// 			</Table>
+			// 		</Card>
+			// 	</SafeAreaView>
+			// )
 		}
 	}
+
 
 	return (
 		<SafeAreaView>
@@ -114,6 +175,8 @@ console.log(workout);
 
 	);
 }
+
+
 
 
 const styles = StyleSheet.create({
@@ -158,6 +221,16 @@ const styles = StyleSheet.create({
 	workoutDetails: {
 		fontSize: 20,
 		paddingLeft: 60
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		margin: 20,
+	},
+	button: {
+		backgroundColor: 'green',
+		width: '40%',
+		height: 40
 	}
 });
 
--- src/screens/MessagesScreen.js
+++ src/screens/MessagesScreen.js
@@ -1,99 +1,117 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity } from 'react-native';
 import useChatResults from '../hooks/chatResults';
 import SearchBar from '../components/SearchBar';
 import firebaseSDK from '../config/firebaseSDK';
+import firebase from 'firebase'
 
-const MessagesScreen = ({navigation}) => {
+const MessagesScreen = ({ navigation }) => {
 
     const [getChatIDs, results, errorMessage] = useChatResults();
 
     const [searchEmail, setSearchEmail] = useState('');
-    
-    const stringToHash = (string) => { 
-                  
-		var hash = 0; 
-		  
-		if (string.length == 0) return hash; 
-		  
-		for (i = 0; i < string.length; i++) { 
-			char = string.charCodeAt(i); 
-			hash = ((hash << 5) - hash) + char; 
-			hash = hash & hash; 
-		} 
-		  
-		return hash; 
+    const [trainerEmail, setTrainerEmail] = useState('');
+
+    const stringToHash = (string) => {
+
+        var hash = 0;
+
+        if (string.length == 0) return hash;
+
+        for (i = 0; i < string.length; i++) {
+            char = string.charCodeAt(i);
+            hash = ((hash << 5) - hash) + char;
+            hash = hash & hash;
+        }
+
+        return hash;
     }
 
-	const createChat = (email) => {
+    const createChat = (email) => {
 
-		var emailtemp = email.toLowerCase();
-		emailtemp = emailtemp.replace(/\./g, ',');
+        var emailtemp = email.toLowerCase();
+        emailtemp = emailtemp.replace(/\./g, ',');
 
-		firebaseSDK.ref("users/" + emailtemp).once("value").then(function(snapshot){
-			if (snapshot.exists()){ // email exists
-				newChat(email);
-			} else {
-				alert("email does not exist");
-			}
-		});
+        firebaseSDK.ref("users/" + emailtemp).once("value").then(function (snapshot) {
+            if (snapshot.exists()) { // email exists
+                newChat(email);
+            } else {
+                alert("Email does not exist");
+            }
+        });
     }
-    
+
     const newChat = (targetEmail) => {
-		const current_user_email = firebaseSDK.email;
-		var userEmail = current_user_email.toLowerCase();
+        const current_user_email = firebaseSDK.email;
+        var userEmail = current_user_email.toLowerCase();
         userEmail = userEmail.replace(/\./g, ',');
 
         var temp_targetEmail = targetEmail.toLowerCase();
         temp_targetEmail = temp_targetEmail.replace(/\./g, ',');
 
-        const chatid = stringToHash(userEmail+targetEmail);
+        const chatid = stringToHash(userEmail + targetEmail);
 
-		var chat = { 
-			user1: current_user_email,
-			user2: targetEmail
-		}
+        var chat = {
+            user1: current_user_email,
+            user2: targetEmail
+        }
 
-		firebaseSDK.createChat(chat, chatid);
-		firebaseSDK.addChatToUser(userEmail, chatid, targetEmail);
+        firebaseSDK.createChat(chat, chatid);
+        firebaseSDK.addChatToUser(userEmail, chatid, targetEmail);
         firebaseSDK.addChatToUser(temp_targetEmail, chatid, current_user_email);
     }
-    
+
     const clearText = () => {
         setSearchEmail('');
     }
 
+    var userEmail = firebaseSDK.email.toLowerCase();
+    userEmail = userEmail.replace(/\./g, ',');
+	firebase.database().ref('/users/' + userEmail).once('value').then(
+		function (snapshot) {
+			partnerEmail = snapshot.val().partnerEmail;
+			setTrainerEmail(partnerEmail);
+		}
+	);
 
-	return (
-		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <SearchBar 
-                term = {searchEmail}
-                onTermChange = {setSearchEmail}
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <SearchBar
+                term={searchEmail}
+                onTermChange={setSearchEmail}
             />
             <Button
-                title = 'Create Chat'
-                onPress = {() => {
+                title='Create Chat'
+                onPress={() => {
                     createChat(searchEmail);
-                    clearText();              
+                    clearText();
                 }}
             />
-            <View style = {styles.container}>
+            <Button
+                title= {'Chat with your trainer ' + trainerEmail}
+                onPress={() => {
+                    createChat(trainerEmail);
+                    clearText();
+                }}
+            />
+            <View style={styles.container}>
 
             </View>
             <FlatList
-                data = {results}
-                keyExtractor = {(result) => String(result.chatid)} // important for performance
-                renderItem = {({item}) => { 
+                data={results}
+                keyExtractor={(result) => String(result.chatid)} // important for performance
+                renderItem={({ item }) => {
                     return (<TouchableOpacity
-                        style = {styles.button}
-                        onPress = {() => {navigation.navigate('ChatScreen', {chatid: item.chatid})}}
-                    > 
-                        <Text>Chat with {item.user}</Text> 
+                        style={styles.button}
+                        onPress={() => { navigation.navigate('ChatScreen', { chatid: item.chatid }) }}
+                    >
+                        <Text>Chat with {item.user}</Text>
                     </TouchableOpacity>)
                 }}
             />
-		</View>
-	);
+        </View>
+    );
 }
 
 
@@ -102,12 +120,12 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#DDDDDD',
         padding: 10
-      }, 
-      container: {
+    },
+    container: {
         flex: 1,
         justifyContent: 'center',
         paddingHorizontal: 10
-      }
+    }
 
 });
 
